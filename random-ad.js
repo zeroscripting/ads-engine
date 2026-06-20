eddbbc49f920e7e82b2a117d5b1bb005a1d99efb
zeroscripting <nandangbaihaqi300@gmail.com>
2026-06-20 15:53:03 +0700

    Update random-ad.js

--- random-ad.js
+++ random-ad.js
@@ -5,16 +5,10 @@
         sessionStorage.setItem(REDIRECT_KEY, 'true');
         
         var adUrls = [
-            "https://portslocked.com/i2mgem9vy?key=8a8bd140f0ee1efdcd032106d8740e72",
-            "https://portslocked.com/rixk3tk53i?key=8564791524ae298b05f9dc1c31140b21",
-            "https://portslocked.com/mpm95ha6q?key=c3256382f8639638827d1bdafc643158",
-            "https://portslocked.com/fa6fp8ha?key=c9fd1421c83bef85b3ac34e1599d356d",
-            "https://portslocked.com/w5mkurw9i6?key=7a2e82e5705b42514967e4a9866dc9cf",
-            "https://portslocked.com/p6wqd9jvgt?key=d51aa763b3cba29348c0fdaf0d597439",
-            "https://portslocked.com/x3yvaqi19?key=ec08d6c47e240103bd41f0f2db4e0467",
-            "https://portslocked.com/kr9sknv3?key=cba6d8dd077a28f98b85731fce1ffdae",
-            "https://portslocked.com/sz8x7x0z?key=3243f6a58e9bec188436f3ddce99e7c9",
-            "https://portslocked.com/q6jhtwa9?key=8a36634a909c95254582023f134f9930"
+            "https://www.effectivecpmnetwork.com/gyunxhcx6e?key=a4140cd38d33a96eb3a9581d555f503a",
+            "https://www.effectivecpmnetwork.com/z0pnsjfsuf?key=5c1e446bfc5f7e9662bcd587f7ae3b36",
+            "https://www.effectivecpmnetwork.com/v5kjpv2d7?key=2297afd37d196afc41fbb6fea531da30",
+            "https://www.effectivecpmnetwork.com/fmbq5tt9wt?key=c2a02ba43bf7fb3024a6f2c6762e5403"
         ];
         
         var randomUrl = adUrls[Math.floor(Math.random() * adUrls.length)];
